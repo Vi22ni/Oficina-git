6385fba8f7daa5b313ab2451dd8a2d67c45c5f06
vi22ni <vinicius22bueno@gmail.com>
2022-02-16 03:56:24 -0300

    Add página ADM, mudei css

---
+++ oficina/js/crud.js
@@ -0,0 +1,10 @@
+let nameForm = document.getElementById('ipt-nome');
+let emailForm = document.getElementById('ipt-email');
+let telephoneForm = document.getElementById('ipt-telefone');
+let messageForm = document.getElementById('mensagem');
+
+let contactTypeEmail = document.getElementById('radio-email');
+let contactTypeWhatsapp = document.getElementById('radio-whatsapp');
+let contactTypeTelephone = document.getElementById('radio-telefone');
+
+let contactTimeCourse = document.getElementById('slc-horario');
